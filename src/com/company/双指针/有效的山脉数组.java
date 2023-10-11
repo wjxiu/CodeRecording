124538a45136c222725d128cfdd458feeaddba55
wjxiu <wujinxiu2002@gmail.com>
2023-10-11 12:04:33 +0800

    ---

--- src/com/company/双指针/有效的山脉数组.java
+++ src/com/company/双指针/有效的山脉数组.java
@@ -33,17 +33,4 @@ public class 有效的山脉数组 {
         if(left!=0&&right!=arr.length-1&&left==right) return true;
         return false;
     }
-    public int maxProfit(int k, int[] prices) {
-        int[][] dp=new int[prices.length][(2*k)+1];
-        for(int i=1;i<prices.length;i+=2){
-            dp[0][i]=-prices[0];
-        }
-        for(int i=1;i<prices.length;i++){
-            for(int j=1;j<=(2*k)+1;j++){
-                if(j%2==1) dp[i][j]=Math.max(dp[i][j],dp[i-1][j-1]-prices[i]);
-                else dp[i][j]=Math.max(dp[i][j],dp[i-1][j-1]+prices[i]);
-            }
-        }
-        return  Arrays.stream(dp[prices.length-1]).max().getAsInt();
-    }
 }
