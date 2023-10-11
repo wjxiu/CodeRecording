903a08f103dec1f663de97105e9e32a4189d55eb
wjxiu <wujinxiu2002@gmail.com>
2023-10-11 11:56:50 +0800

    背包问题+双指针

--- src/com/company/双指针/有效的山脉数组.java
+++ src/com/company/双指针/有效的山脉数组.java
@@ -1,5 +1,7 @@
 package com.company.双指针;
 
+import java.util.Arrays;
+
 /**
  * @author xiu
  * @create 2023-10-10 14:36
@@ -30,5 +32,18 @@ public class 有效的山脉数组 {
         while (right-1>=0&&arr[right-1]>arr[right]) if (arr[right-1]>arr[right]) right--;
         if(left!=0&&right!=arr.length-1&&left==right) return true;
         return false;
+    }
+    public int maxProfit(int k, int[] prices) {
+        int[][] dp=new int[prices.length][(2*k)+1];
+        for(int i=1;i<prices.length;i+=2){
+            dp[0][i]=-prices[0];
+        }
+        for(int i=1;i<prices.length;i++){
+            for(int j=1;j<=(2*k)+1;j++){
+                if(j%2==1) dp[i][j]=Math.max(dp[i][j],dp[i-1][j-1]-prices[i]);
+                else dp[i][j]=Math.max(dp[i][j],dp[i-1][j-1]+prices[i]);
+            }
+        }
+        return  Arrays.stream(dp[prices.length-1]).max().getAsInt();
     }
 }
